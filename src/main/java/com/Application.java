fbb6b9c6ffbffde1606d0defc180273c5ea8845d
yenhsin <a3909117000@gmail.com>
2025-08-02 23:05:23 +0800

    chore: 跨域設定

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,6 +2,9 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,18 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedOrigins("http://localhost:5173")
+	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                        .allowCredentials(true);
+	            }
+	        };
+	    }
 
 }
