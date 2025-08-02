7a8fdf68fba6fcbfa0ddd104d6ed7007c41d9edc
yenhsin <a3909117000@gmail.com>
2025-08-02 19:50:15 +0800

    feat: 使用者登入功能

---
+++ src/main/java/com/user/model/UserDTO_login.java
@@ -0,0 +1,31 @@
+package com.user.model;
+
+public class UserDTO_login{
+	
+
+	private String phone; //手機
+	private String password; //使用者密碼
+		
+	public UserDTO_login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
--- src/main/java/com/user/model/UserService.java
+++ src/main/java/com/user/model/UserService.java
@@ -16,19 +16,29 @@ public class UserService {
 	private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 	
 	// 註冊使用者
-		public UserVO addUser(UserDTO_register dto) throws IOException {
+	public UserVO addUser(UserDTO_register dto) throws IOException {
 
-			
-			byte[] imageBytes = dto.getCoverImg() != null ? dto.getCoverImg().getBytes() : null;
+		byte[] imageBytes = dto.getCoverImg() != null ? dto.getCoverImg().getBytes() : null;
 
-			UserVO user = new UserVO();
-			user.setUserName(dto.getUserName());
-			user.setEmail(dto.getEmail());
-			user.setPassword(encoder.encode(dto.getPassword()));
-			user.setPhone(dto.getPhone());
-			user.setCoverImg(imageBytes);
-			user.setBio(dto.getBio());
-			return userRepo.save(user);
-		}
+		UserVO user = new UserVO();
+		user.setUserName(dto.getUserName());
+		user.setEmail(dto.getEmail());
+		user.setPassword(encoder.encode(dto.getPassword()));
+		user.setPhone(dto.getPhone());
+		user.setCoverImg(imageBytes);
+		user.setBio(dto.getBio());
+		return userRepo.save(user);
+	}
+	
+	//登入
+	public UserVO login(UserDTO_login dto) {
+		UserVO user = userRepo.findByPhone(dto.getPhone());
+		if(user == null)
+			throw new RuntimeException("找不到此電話號碼");
+		if(!encoder.matches(dto.getPassword(), user.getPassword())) //比對使用者傳入的原始密碼和資料庫的加密密碼
+			throw new RuntimeException("密碼錯誤");
+		
+		return user;
+	}
 
 }
